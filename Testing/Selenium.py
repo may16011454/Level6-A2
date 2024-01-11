a62da571e1ae43d3377eeb18dfac74edc1601a53
Logan <mayylogan@gmail.com>
2024-01-11 15:56:11 +0000

    Added Selenium Testing
    moved all files into an app folder to keep the application and testing in different areas

---
+++ Testing/Selenium.py
@@ -0,0 +1,88 @@
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import Select
+import time
+
+
+#Selenium code to register a user then add another user through the admin dashboard
+
+# Launch Chrome
+driver = webdriver.Chrome()
+
+# Open the website
+driver.get('http://localhost/Level6-A2/app/index.php') 
+
+# Create a new account
+register_link = driver.find_element(By.XPATH, '/html/body/div[2]/div/div[2]/div/div/a')
+register_link.click()
+
+# Fill in registration details
+firstname_input = driver.find_element(By.ID, 'fname')
+firstname_input.send_keys('TestUser')
+
+lastname_input = driver.find_element(By.ID, 'sname')
+lastname_input.send_keys('Selenium')
+
+email_input = driver.find_element(By.ID, 'email')
+email_input.send_keys('Selenium@tests.com')
+
+password_input = driver.find_element(By.ID, 'password')
+password_input.send_keys('Password.11')
+
+confirm_password_input = driver.find_element(By.ID, 'password-v')
+confirm_password_input.send_keys('Password.11')
+
+# Submit  register form
+register_button = driver.find_element(By.XPATH, '/html/body/form/section/div/div/div/div/div/button')
+register_button.click()
+
+time.sleep(2)
+
+# Log in as admin
+# Fill in login details
+login_email_input = driver.find_element(By.ID, 'email')
+login_email_input.send_keys('admin@admin.com') 
+
+login_password_input = driver.find_element(By.ID, 'password')
+login_password_input.send_keys('Password.11') 
+
+# Submit the login form
+login_button = driver.find_element(By.XPATH, '/html/body/form/section/div/div/div/div/div/button')
+login_button.click()
+
+time.sleep(2)
+
+# Go to the add user page
+add_user_link = driver.find_element(By.XPATH, '/html/body/div/div/div[1]/div/div[2]/a')
+add_user_link.click()
+
+# Add user button
+add_user_button = driver.find_element(By.XPATH, '/html/body/div/a')
+add_user_button.click()
+
+# Fill in information
+firstname_input = driver.find_element(By.ID, 'firstname')
+firstname_input.send_keys('Selenium')
+
+lastname_input = driver.find_element(By.ID, 'lastname')
+lastname_input.send_keys('Testerererer')
+
+email_input = driver.find_element(By.ID, 'email')
+email_input.send_keys('selenium@testinger.com')
+
+password_input = driver.find_element(By.ID, 'password')
+password_input.send_keys('Password.11')
+
+# Select role from the dropdown
+role_dropdown = Select(driver.find_element(By.ID, 'role'))
+role_dropdown.select_by_value('2') 
+
+# Submit  form
+submit_button = driver.find_element(By.XPATH, '/html/body/form/section/div/div/div/div/div/button')
+submit_button.click()
+
+time.sleep(2)
+
+# Close the browser
+driver.quit()
--- app/components/reg-form.php
+++ app/components/reg-form.php
@@ -6,14 +6,13 @@ require_once './inc/functions.php';
 $message = isset($_GET['error']) ? htmlspecialchars($_GET['error']) : '';
 
 // Check if the form is submitted via POST
-if ($_SERVER['REQUEST_METHOD'] == 'POST')
-{
+if ($_SERVER['REQUEST_METHOD'] == 'POST') {
   // Process the submitted form data
   $fname = InputProcessor::processString($_POST['fname']);
   $sname =  InputProcessor::processString($_POST['sname']);
   $email =  InputProcessor::processEmail($_POST['email']);
   $password =  InputProcessor::processPassword($_POST['password'], $_POST['password-v']);
-  
+
   // Validate all inputs
   $valid = $fname['valid'] && $sname['valid'] && $email['valid'] && $password['valid'];
 
@@ -21,13 +20,14 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
   $message = !$valid ? "Please fix the above errors:" : '';
 
   // If all inputs are valid, proceed with registration
-  if ($valid)
-  {
+  if ($valid) {
     // Prepare the data for registration
-    $args = ['firstname' => $fname['value'],
-             'lastname' => $sname['value'],
-             'email' => $email['value'],
-             'password' => password_hash($password['value'], PASSWORD_DEFAULT)];
+    $args = [
+      'firstname' => $fname['value'],
+      'lastname' => $sname['value'],
+      'email' => $email['value'],
+      'password' => password_hash($password['value'], PASSWORD_DEFAULT)
+    ];
 
     // Register the member
     $member = $controllers->members()->register_member($args);
@@ -38,9 +38,7 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
       // Set error message if email is already registered
       $message = "Email already registered.";
     }
-    
   }
-
 }
 ?>
 
@@ -55,34 +53,34 @@ if ($_SERVER['REQUEST_METHOD'] == 'POST')
 
               <h3 class="mb-2">Register</h3>
               <div class="form-outline mb-4">
-                <input required type="text" id="fname" name="fname" class="form-control form-control-lg" placeholder="Firstname" value="<?= htmlspecialchars($fname['value'] ?? '') ?>"/>
+                <input required type="text" id="fname" name="fname" class="form-control form-control-lg" placeholder="Firstname" value="<?= htmlspecialchars($fname['value'] ?? '') ?>" />
                 <small class="text-danger"><?= htmlspecialchars($fname['error'] ?? '') ?></small>
               </div>
 
               <div class="form-outline mb-4">
-                <input required type="text" id="sname" name="sname" class="form-control form-control-lg" placeholder="Surname" value="<?= htmlspecialchars($sname['value'] ?? '') ?>"/>
+                <input required type="text" id="sname" name="sname" class="form-control form-control-lg" placeholder="Surname" value="<?= htmlspecialchars($sname['value'] ?? '') ?>" />
                 <small class="text-danger"><?= htmlspecialchars($sname['error'] ?? '') ?></small>
               </div>
 
 
               <div class="form-outline mb-4">
-                <input required type="email" id="email" name="email" class="form-control form-control-lg" placeholder="Email" value="<?= htmlspecialchars($email['value']?? '') ?>" />
+                <input required type="email" id="email" name="email" class="form-control form-control-lg" placeholder="Email" value="<?= htmlspecialchars($email['value'] ?? '') ?>" />
                 <small class="text-danger"><?= htmlspecialchars($email['error'] ?? '') ?></small>
               </div>
 
               <div class="form-outline mb-4">
                 <input required type="password" id="password" name="password" class="form-control form-control-lg" placeholder="Password" />
               </div>
-              
+
               <div class="form-outline mb-4">
                 <input required type="password" id="password-v" name="password-v" class="form-control form-control-lg" placeholder="Password again" />
                 <small class="text-danger"><?= htmlspecialchars($password['error'] ?? '') ?></small>
               </div>
 
               <button class="btn btn-primary btn-lg w-100 mb-4" type="submit">Register</button>
-              <a class="btn btn-secondary btn-lg w-100" type="submit" href="./login.php" >Already got an account?</a>
+              <a class="btn btn-secondary btn-lg w-100" type="submit" href="./login.php">Already got an account?</a>
 
-              <?php if ($message): ?>
+              <?php if ($message) : ?>
                 <div class="alert alert-danger mt-4">
                   <?= $message ?? '' ?>
                 </div>
